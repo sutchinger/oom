6b4f2d219b98a963f635cd4ad120baeb13b82c7f
Wolfgang <sutch@gmx.at>
2017-04-25 20:21:41 +0200

    add project Task1

---
+++ tasks/task1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+
+            return 0;
+        }
+    }
+}
