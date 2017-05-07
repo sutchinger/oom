c3dbfa9f7e4c92fd818208e68c27572f5c6aedee
Wolfgang <sutch@gmx.at>
2017-05-07 15:10:07 +0200

    add Uebung 2

---
+++ Uebung/LE2/LE2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LE2
+{
+    class Book
+    {
+        private string title;
+        private decimal price;
+
+        public Book(string newTitle, decimal newPrice)
+        {
+            
+            if (newTitle == null)
+                throw new ArgumentException("nix");
+            title = newTitle;
+            Price = newPrice;
+        }
+        public Book(string newTitle) : this(newTitle, 0) // ein Constructor mit anderem Constructor aufrufen
+        {
+        }
+        public Book() : this("default", 0) { }
+
+        //public string getTitle() =>  title; //eigentlich { return title;}
+        /*public decimal getPrice() => price; 
+        public void setPrice(decimal newPrice)
+        {
+            if (newPrice < 0)
+                throw new ArgumentException("negativer Preis");
+            price = newPrice;
+        }
+        */
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("negativer Preis");
+                price = value;  // value = keyword für Zuweisung
+            }
+        }
+        /*
+         * public string Title     //geht auch nur get
+        {
+            get
+            {
+                return title;
+            }
+        }
+        */
+        public string Title => title; // gleiches wie oben, = "getter" property
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book a = new Book("Mein erstes Buch", 10);
+            Book c = a; // gleiche Referenz, keine Kopie
+
+            Book b = new Book("ABC"); //Überladen von Funktionen
+            Book d = new Book();
+
+
+
+
+
+
+            /*
+            a.price = -10; // wenn public sichtbar, sonst syntaxfehler
+            Console.WriteLine("Buchpreis " + a.price);
+            */
+            Console.WriteLine($"Buchpreis {a.Price} Name {a.Title} ");  // $ zum Einsetzen
+
+
+
+        }
+    }
+}
---
+++ Uebung/lesson2/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            int a = 5;
+            string hello = "Hello World!";
+            double x = 123.456;
+            bool b = true;
+            string hello = "Hello World!";
+            //Console.BackgroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("{0}\n", hello);
+
+
+            // strings and other values can be concatenated using `+`.
+            Console.WriteLine("The value of x*a is " + (x * a) + ", and b is " + b + ".");
+
+            // ... or you can use place holders ...
+            Console.WriteLine("The value of x*a is {0}, and b is {1}.", x * a, b);
+
+            // ... or string interpolation
+            Console.WriteLine($"The value of x*a is {x * a}, and b is {b}.");
+
+            int[] xs = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int firstElement = xs[0];
+            xs[9] = 100;
+
+
+            var list1 = new List<string> { "This", "is", "a", "list", "of", "strings", "!" };
+            var list2 = new List<int> { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            list1.Add("Hello World.");
+            list2.Add(100);
+
+
+
+            return 0;
+        }
+    }
+}
