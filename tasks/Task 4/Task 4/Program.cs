dacd428d5f91eba67b28e0edd30752b9aae5a8da
sutchinger <sutchinger@gmail.com>
2017-05-19 17:30:31 +0200

    Add projekt task4

---
+++ tasks/Task 4/Task 4/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using static System.Console;
+using System.IO;
+
+namespace task_4
+{
+    public interface IGrab
+    {
+        string Name { get; set; }
+        int Index { get; set; }
+        string Typ { get; }
+        void MachInschrift();
+        
+
+    }
+    
+    class Pyramide : IGrab
+    {
+        public Pyramide(string newName, int newIndex)
+        {
+            Name = newName;
+            {
+                if(Name == "")
+                {
+                    Name = "Mumie X";
+                }
+            }
+            Index = newIndex;
+            {
+                Index = Index > 0 ? Index : 1;
+
+            }
+        }
+
+        public Pyramide()
+        {
+            throw new Exception("keine Parameter übergeben");
+        }
+
+        public string Name { get; set; }
+        public int Index { get; set; }
+        public string Typ => "Pyramide";
+        public void MachInschrift()
+        {
+            WriteLine("Sklavenarmee wird herangetrieben");
+        }
+        
+
+
+    }
+
+    class ErdGrab : IGrab
+    {
+        public ErdGrab(string newName, int newIndex)
+        {
+            Name = newName;
+            {
+                if (Name == "")
+                {
+                    Name = "John Doe";
+                }
+            }
+            Index = newIndex;
+            {
+                Index = Index > 0 ? Index : 1;
+
+            }
+        }
+        public ErdGrab()
+        {
+            throw new Exception("keine Parameter übergeben");
+        }
+
+        public int Index { get; set; }
+        public string Name { get; set; }
+        
+        public string Typ => "Erdgrab";
+        public void MachInschrift()
+        {
+            WriteLine("Steinmetz wird beauftragt");
+        }
+        
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Pyramide a = new Pyramide("Ramses",1);
+            Pyramide b = new Pyramide("TutEnchAmun",2);
+            Pyramide c = new Pyramide("Echnaton",8);
+            ErdGrab d = new ErdGrab("Mayer",4);
+            ErdGrab e = new ErdGrab("Suchy",12);
+            ErdGrab f = new ErdGrab("Sandler",7);
+
+            
+            IGrab[] array = new IGrab[] { a, b, c, d, e, f };
+
+            /// Array ausgeben
+            /// Array sortieren
+            Array.Sort(array, delegate (IGrab x, IGrab y) {
+                return x.Index.CompareTo(y.Index);
+            });
+            foreach (var x in array)
+            {
+
+                WriteLine($"{x.Name} auf Nummer {x.Index} in einem Typ {x.Typ}; zum Inschrift erstellen: ");
+                x.MachInschrift();
+                WriteLine("\n");
+            }
+
+            /// Serialisieren / Deserialisieren
+            string GrabListe1 = JsonConvert.SerializeObject(array);
+            WriteLine("\nGrabliste1:\n" + GrabListe1);
+            //File.Create(@"C:\gkkdfu\test.txt");
+            try
+            {
+                File.WriteAllText(Path.GetTempPath() + "\\test.txt", GrabListe1);
+            }
+            catch (UnauthorizedAccessException p)
+            {
+                WriteLine(p.Message);
+            }
+            string GrabListe2 = File.ReadAllText(Path.GetTempPath() + "\\test.txt");
+
+            WriteLine("\nGrabliste 2:\n" + GrabListe2);
+            
+
+
+            string P1 = JsonConvert.SerializeObject(a);
+            WriteLine("\n****"+P1+"***\n");
+
+            Pyramide z = JsonConvert.DeserializeObject<Pyramide>(P1);
+            WriteLine($"{z.Name} liegt auf Nummer {z.Index} in einem Typ {z.Typ}");
+
+
+            /// Manuelle Grab Vergabe
+            char grabWahl = 'y';
+            var neu = new List<IGrab>();
+            
+            string tempName;
+            int tempIndex = 0;
+            
+            do
+            {
+                WriteLine("\nNeues Grab vergeben:\nE: ErdGrab\nP: Pyramide\nx: beenden");
+                /// Auswahl einlesen und Exeption Handling
+                try
+                {
+                    grabWahl = char.Parse(ReadLine());
+                    //grabWahl = ReadKey().KeyChar;
+                }
+                catch (FormatException p)
+                {
+                    WriteLine(p.Message);
+                }
+              
+                switch (grabWahl)
+                {
+                    case 'p':
+                    case 'P':
+
+                        tempName = GrabEingabe(ref tempIndex);
+                        neu.Add(new Pyramide(tempName, tempIndex));
+                        break;
+                    case 'e':
+                    case 'E':
+                        tempName = GrabEingabe(ref tempIndex);
+
+                        neu.Add(new ErdGrab(tempName, tempIndex));
+
+                        break;
+                    case 'x':
+                        break;
+                    case 'y':
+                        break;
+                    default:
+                        WriteLine("\nThese are not the chars you are looking for");
+                        grabWahl = 'y';
+                        break;
+                }
+
+            } while (grabWahl != 'x');
+            /// manuell erstellte Gräber ausgeben
+            /// Liste sortieren
+            neu.Sort((x,y) => x.Index.CompareTo(y.Index));
+            foreach (var y in neu)
+            {
+
+                WriteLine($"{y.Name} auf Nummer {y.Index} in einem Typ {y.Typ}; zum Inschrift erstellen: ");
+                y.MachInschrift();
+                WriteLine("\n");
+            }
+
+            //WriteLine($"{a.Name} liegt auf Nummer {a.Index} in einem Typ {a.Typ}");
+
+
+        }
+
+        private static string GrabEingabe(ref int tempIndex)
+        {
+            string tempName;
+            Write("\nName Verstorbener: ");
+            tempName = ReadLine();
+            Write("\nIndexnummer Grabstelle: ");
+            /// string int Parse Exeption handling
+            try
+            {
+                tempIndex = int.Parse(ReadLine());
+            }
+            catch (FormatException p)
+            {
+                WriteLine(p.Message);
+            }
+
+            return tempName;
+        }
+    }
+}
---
+++ tasks/Task 4/Task 4/Tests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using task_4;
+using System.IO;
+
+namespace Task_4
+{
+    [TestFixture]
+
+    class Tests
+    {
+
+        [Test]
+        public void KeinIndexKleinerNull()
+        {
+            var x = new Pyramide("Hans", -1);
+            Assert.True(x.Index > 0);
+            var y = new ErdGrab("Hans", -1);
+            Assert.True(y.Index > 0);
+        }
+
+        [Test]
+        public void NoName()
+        {
+            var x = new ErdGrab("", 1);
+            var y = new Pyramide("", 1);
+            Assert.True(x.Name != "");
+            Assert.False(y.Name == "");
+        }
+
+        [Test]
+        public void CanCreatePyramide()
+        {
+            var x = new Pyramide("Ramses", 1);
+            Assert.IsTrue(x.Name == "Ramses");
+            Assert.IsTrue(x.Index == 1);
+            Assert.IsTrue(x.Typ == "Pyramide");
+        }
+
+        [Test]
+        public void CanCreateErdgrab()
+        {
+            var x = new ErdGrab("Ramses", 1);
+            Assert.IsTrue(x.Name == "Ramses");
+            Assert.IsTrue(x.Index == 1);
+            Assert.IsTrue(x.Typ == "Erdgrab");
+        }
+
+        [Test]
+        ///Console Out Test!!!
+        public void SklavenKommen()
+        {
+            var x = new Pyramide("Ramses", 1);
+            using (StreamWriter writer = new StreamWriter(Path.GetTempPath() + "\\test.txt"))
+            {
+                Console.SetOut(writer);
+                x.MachInschrift();
+            }
+            string result = File.ReadAllText(Path.GetTempPath() + "\\test.txt");
+            Assert.IsTrue(result.StartsWith("Sklaven"));
+        }
+        [Test]
+        public void ArraySortByIndexPossible()
+        {
+            Pyramide a = new Pyramide("Echnaton", 8);
+            Pyramide b = new Pyramide("Ramses", 2);
+            Pyramide c = new Pyramide("TutEnchAmun", 1);
+            IGrab[] array = new IGrab[] { a, b,c };
+            Array.Sort(array, delegate (IGrab x, IGrab y) {
+                return x.Index.CompareTo(y.Index);
+            });
+            Assert.IsTrue(array[0].Index == 1);
+        }
+
+        [Test]
+        public void CatchPyramideWithoutParameter()
+        {
+            Assert.Catch(() => { var x = new Pyramide(); });
+            
+        }
+
+        [Test]
+        public void CatchErdgrabWithoutParameter()
+        {
+            Assert.Catch(() => { var x = new ErdGrab(); });
+
+        }
+
+    }
+}
